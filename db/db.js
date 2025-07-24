325948397902218f8f14048843231012f061a018
lim0808 <146072293+lim0808@users.noreply.github.com>
2025-07-24 15:49:48 +0800

    v1.0

--- db/db.js
+++ db/db.js
@@ -9,11 +9,12 @@ const mysql = require('mysql2');
 // });
 
 const pool = mysql.createPool({
-  host: 'mysql2.sqlpub.com:3307',
+  host: 'mysql2.sqlpub.com',
+  port: 3307,
   user: 'root_12',
   password: '1dvXki1ihwjeCWWh',
   database: 'mydb_lim_1',
   timezone: '+08:00'
-});
+})
 
 module.exports = pool.promise(); // 使用 Promise 语法
