ddd04297403bb38e96fc8ab0278168191bd5c48e
lim0808 <146072293+lim0808@users.noreply.github.com>
2025-07-24 15:47:12 +0800

    v1.0

--- db/db.js
+++ db/db.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'express',
+//   timezone: '+08:00'
+// });
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'express',
+  host: 'mysql2.sqlpub.com:3307',
+  user: 'root_12',
+  password: '1dvXki1ihwjeCWWh',
+  database: 'mydb_lim_1',
   timezone: '+08:00'
 });
 
